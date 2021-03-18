e8fd362a0f6cc7240a23226cf495134137c41111
ahmed.hamma <ahmed.hamma@le-campus-numerique.fr>
2021-03-18 16:33:57 +0100

    début menu

--- src/main/menu.java
+++ src/main/menu.java
@@ -1,5 +1,16 @@
 package main;
-
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class menu {
+    public static void main (String args[]){
+        String nouveauPerso;
+        String quitter;
+        Scanner menu = new Scanner(System.in);
+        System.out.print("Crée ton perso bg");
+        nouveauPerso = menu.nextLine();
+        System.out.println(nouveauPerso);
 
+        System.out.println("ou tu peux partir!");
+        menu.close();
+    }
 }
