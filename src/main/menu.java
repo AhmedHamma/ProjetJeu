8013f9cb6e77f6eb15c40f4f6f20cf790f0efc9d
ahmedHamma <ahmed.hamma@le-campus-numerique.fr>
2021-03-22 20:17:53 +0100

    wip

--- src/main/menu.java
+++ src/main/menu.java
@@ -1,7 +1,9 @@
 package main;
 import java.sql.SQLOutput;
 import java.util.Scanner;
+
 public class menu {
+
     public static void main (String args[]){
 
 
@@ -11,10 +13,24 @@ public class menu {
         Integer Input = menu.nextInt();
         if (Input.equals(1)){
             System.out.println("création de perso");
+            System.out.println("1 :Guerrier");
+            System.out.println("2 :Mage");
+            Integer choixPerso = menu.nextInt();
+                if(choixPerso.equals(1)){
+                    guerrier Guerrier = new guerrier();
+                    System.out.println(Guerrier.getNom()); // instancier l'objet de la classe guerrier
+            }
+                if(choixPerso.equals(2)){
+                    mage Mage = new mage();
+                    System.out.println(Mage.getNom());
+            }
         }
         if (Input.equals(2)){
             System.out.println("dégage");
         }
 
+
     }
+
+
 }
