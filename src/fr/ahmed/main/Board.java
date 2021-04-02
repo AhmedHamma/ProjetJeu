5500895ffb6c3acb55e506046309269e6e889a9b
ahmed.hamma <ahmed.hamma@le-campus-numerique.fr>
2021-04-02 16:53:32 +0200

    Itération 6

--- src/fr/ahmed/main/Board.java
+++ src/fr/ahmed/main/Board.java
@@ -14,6 +14,9 @@ import fr.ahmed.Enemies.Witcher;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 
 public class Board {
     private ArrayList<Case> board;
@@ -72,33 +75,61 @@ public class Board {
 
     public void fillBoard() {
 
-        for (int i = 0; i <= 64; i++) { // boucle for
-            if ((i == 45) || (i == 52) || (i == 56) || (i == 62)) {
-                board.add(new Dragon("Dragoune", 10, 2));
-            } else if ((i == 3) || (i == 6) || (i == 9) || (i == 12) || (i == 15) || (i == 18) || (i == 21) || (i == 24) || (i == 27) || (i == 30)) {
-                board.add(new Goblin("Goblinkun", 8, 1));
-            } else if ((i == 10) || (i == 20) || (i == 25) || (i == 32) || (i == 35) || (i == 36) || (i == 37) || (i == 40) || (i == 44) || (i == 47)) {
-                board.add(new Witcher("Henry", 6, 3));
-            } else if (i == 48 || i == 49 ) {
-                board.add(new Fireball());
-            } else if ((i == 2) || (i == 11) || (i == 5) || (i == 22) || (i == 29)) {
-                board.add(new Hammer());
-            } else if ((i == 1) || (i == 4) || (i == 8) || (i == 17) || (i == 23)) {
-                board.add(new Thunder("Thunder", 9));
-            } else if ((i == 19) || (i == 26) || (i == 42) || (i == 53)) {
-                board.add(new Sword("Epée", 4));
-            } else if ((i == 7) || (i == 13) || (i == 31) || (i == 33) || (i == 39) || (i == 43)) {
-                board.add(new LittlePotion());
-            } else if ((i == 28) || (i == 41)) {
-                board.add(new SuperPotion());
-            } else {
-                board.add(null);
-            }
+        for (int i = 0; i < 10; i++) {
+            board.add(new Goblin("Goblin-kun", 4, 3));
+            board.add(new Witcher("Henry", 5, 6));
+        }
+        for (int i = 0; i < 6; i++) {
+            board.add(new LittlePotion());
+        }
+        for (int i = 0; i < 5; i++) {
+            board.add(new Thunder());
+            board.add(new Hammer());
+        }
+        for (int i = 0; i < 2; i++) {
+            board.add(new Fireball());
+            board.add(new SuperPotion());
+        }
+        for (int i = 0; i < 4; i++) {
+            board.add(new Dragon("Dragoune", 10, 8));
+            board.add(new Sword());
+        }
+        for (int i = 0; i <17;  i++){
+            board.add(new CaseVide("Vide"));
+        }
+        Collections.shuffle(board);
 
 
         }
+
+
+//        for (int i = 0; i <= 64; i++) { // boucle for
+//            if ((i == 45) || (i == 52) || (i == 56) || (i == 62)) {
+//                board.add(new Dragon("Dragoune", 10, 2));
+//            } else if ((i == 3) || (i == 6) || (i == 9) || (i == 12) || (i == 15) || (i == 18) || (i == 21) || (i == 24) || (i == 27) || (i == 30)) {
+//                board.add(new Goblin("Goblinkun", 8, 1));
+//            } else if ((i == 10) || (i == 20) || (i == 25) || (i == 32) || (i == 35) || (i == 36) || (i == 37) || (i == 40) || (i == 44) || (i == 47)) {
+//                board.add(new Witcher("Henry", 6, 3));
+//            } else if (i == 48 || i == 49 ) {
+//                board.add(new Fireball());
+//            } else if ((i == 2) || (i == 11) || (i == 5) || (i == 22) || (i == 29)) {
+//                board.add(new Hammer());
+//            } else if ((i == 1) || (i == 4) || (i == 8) || (i == 17) || (i == 23)) {
+//                board.add(new Thunder("Thunder", 9));
+//            } else if ((i == 19) || (i == 26) || (i == 42) || (i == 53)) {
+//                board.add(new Sword("Epée", 4));
+//            } else if ((i == 7) || (i == 13) || (i == 31) || (i == 33) || (i == 39) || (i == 43)) {
+//                board.add(new LittlePotion());
+//            } else if ((i == 28) || (i == 41)) {
+//                board.add(new SuperPotion());
+//            } else {
+//                board.add(null);
+//            }
+
+
+//        }
 //        System.out.println(board.size());
 
 
-    }
+//    }
 }
---
+++ src/fr/ahmed/main/CaseVide.java
@@ -0,0 +1,12 @@
+package fr.ahmed.main;
+
+public class CaseVide extends Case {
+    public CaseVide(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Y'a R ici";
+    }
+}
